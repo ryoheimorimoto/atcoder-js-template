debf3f80b3110960c50cc8f9572939af5dabf07c
Ryohei Morimoto <ryoheimorimoto@gmail.com>
2020-01-06 15:46:55 +0900

    Add template and its dependencies.

---
+++ templates/index.js
@@ -0,0 +1,10 @@
+import * as fs from 'fs';
+// import _ from 'lodash';
+// import 'core-js';
+// import bigInt from 'big-integer';
+
+const main = (input) => {
+  
+}
+
+main(fs.readFileSync('/dev/stdin', 'utf8').trim());
